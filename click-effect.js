831a3ff5d09abcaa5d775a3523fa45119979480b
nouser <nouser>
2023-08-08 09:27:45 +0200

    update

---
+++ click-effect.js
@@ -0,0 +1,13 @@
+
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach(input => {
+    input.addEventListener('click', (e) => {
+           inputs.forEach(input => {
+            if(input.checked){
+            input.checked = false;         
+            e.target.checked = true;         
+            }
+        })
+    })
+})
